0c57cfac35efbf290535f4495be53ee70fa65801
Vincent-Lavallee <vincent-lavallee@live.com>
2023-03-04 21:25:05 -0500

    Fixed project descriptions

--- src/features/projects/Projects.ts
+++ src/features/projects/Projects.ts
@@ -6,7 +6,7 @@ const projects = [
     image: "/projects/homa.png",
     repoUrl: "https://github.com/Vincent-Lavallee/homa-frontend",
     description:
-      "This project was a learning experience for me since it's the first time I've looked at c++. This project is a home automation system that I'm currently working on. It's a work in progress but currently I have an arduino which is wired to get and show the temperature of the room it's placed in. It's send all that info via MQTT server where I'm retreiving it via either a website or Mobile app in react native.",
+      "This project was a learning experience for me since it was the first time I've touched at c++. This is an home automation system that I'm currently working on. It's a work in progress, right now I have an arduino which is wired to read and show the temperature of the room it's placed in. It then sends all that info via an MQTT server where I'm retreiving it via either a website or Mobile app ",
     date: new Date("2022-06-20"),
   },
   {
@@ -14,7 +14,7 @@ const projects = [
     image: "/projects/odj.png",
     repoUrl: "https://github.com/Vincent-Lavallee/ODJ",
     description:
-      "This is where it all started. This was my first real project in react and Node. It was quite ambitious at the time since I was still learning react and NodeJs and I wanted something mutliplayer. I learned a lot from this project and It gave me the passion to continue. ",
+      "This is where it all started. This was my first real project in React and NodeJs. It was quite ambitious at the time since I was still learning react and Node and I wanted something mutliplayer which implied I'd have to implement websockets. I still learned a lot from this project and It gave me the passion to continue. ",
     date: new Date("2021-11-11"),
   },
 ] as ProjectModel[];
